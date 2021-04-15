82bed5523562b19fb2f018b6ec5f828a8701d70d
abdojabboulie <abdojabboulie@gmail.com>
2021-04-15 15:41:15 +0200

    i got it to work

---
+++ passport-config.js
@@ -0,0 +1,29 @@
+const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcrypt')
+
+function initialize(passport, getUserByEmail, getUserById) {
+  const authenticateUser = async (email, password, done) => {
+    const user = getUserByEmail(email)
+    if (user == null) {
+      return done(null, false, { message: 'looks like u wrote the wrong email foool' })
+    }
+
+    try {
+      if (await bcrypt.compare(password, user.password)) {
+        return done(null, user)
+      } else {
+        return done(null, false, { message: 'wrong! in trumps voice' })
+      }
+    } catch (e) {
+      return done(e)
+    }
+  }
+
+  passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser))
+  passport.serializeUser((user, done) => done(null, user.id))
+  passport.deserializeUser((id, done) => {
+    return done(null, getUserById(id))
+  })
+}
+
+module.exports = initialize 
